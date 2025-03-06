39b7c6aaaa30f8835c6a64bb7f62d0ee27e5cd04
“Quique” <quiquecampos92@outlook.com>
2025-03-06 12:30:28 +0100

    try render

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ const app = require('./app')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 
-app.listen(config.PORT, () => {
-    logger.info(`Server running on port ${config.PORT}`)
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
 })
