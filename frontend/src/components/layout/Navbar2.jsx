fa8e719f7b96436d9f139ed0b997b9248ff44897
“Quique” <quiquecampos92@outlook.com>
2025-03-24 22:08:31 +0100

    styles modified

--- frontend/src/components/layout/Navbar2.jsx
+++ frontend/src/components/layout/Navbar2.jsx
@@ -27,13 +27,13 @@ export function Navbar2({ onSearch }) {
                     <LogoutButton logout={logout} />
                 </div>
 
-                <div className="flex items-center justify-start max-w-screen-xl pl-4 py-3">
+                <div className="flex items-center justify-center max-w-screen-xl pl-4 py-3">
                     <ul className="flex flex-row font-medium mt-0 space-x-8 text-md">
                         {links
                             .filter(link => link.name)
                             .map(link => {
                                 return (
-                                    <li key={link.name}>
+                                    <li key={link.name} className="transform hover:scale-110 transition-transform duration-300">
                                         <Link
                                             to={link.path}
                                             className={`hover:text-orange-500 pb-2 ${location.pathname === link.path ? "text-orange-500 font-bold border-b-2 border-orange-500 transition-colors duration-500" : "text-gray-500 transition-colors duration-300"}`}
