5aa02f298d33ed736e63603741113c9b9e0f81ea
“Quique” <quiquecampos92@outlook.com>
2025-03-24 21:26:43 +0100

    add green logo in header-navbar and delete styles

--- frontend/src/components/layout/Navbar2.jsx
+++ frontend/src/components/layout/Navbar2.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useContext } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { SearchTerm } from '../forms/SearchTerm.jsx';
-import fullLogo from '../../assets/booksavewhiterow.png';
+import greenfullLogo from '../../assets/booksavegreenrow.png';
 import { LogoutButton } from "../buttons/LogoutButton.jsx";
 import { AuthContext } from "../../AuthContext.jsx";
 
@@ -18,10 +18,10 @@ export function Navbar2({ onSearch }) {
 
     return (
         <>
-            <nav className="bg-gradient-to-b from-green-600 to-green-0 px-5">
+            <nav className=" px-5">
                 <div className={`flex justify-between items-center p-4`}>
                     <Link to="/books">
-                        <img src={fullLogo} alt="logo booksave" className="w-32" />
+                        <img src={greenfullLogo} alt="logo booksave" className="w-32" />
                     </Link>
                     <SearchTerm onSearch={onSearch} />
                     <LogoutButton logout={logout} />
