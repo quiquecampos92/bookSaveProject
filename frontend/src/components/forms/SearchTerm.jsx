b48d71beacd0dfb2ca1318919300cb44aa05a33f
“Quique” <quiquecampos92@outlook.com>
2025-03-25 17:29:23 +0100

    add auto search without enter

--- frontend/src/components/forms/SearchTerm.jsx
+++ frontend/src/components/forms/SearchTerm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import bookService from "../../services/books.js";
 
 export function SearchTerm({ onSearch }) {
@@ -7,27 +7,36 @@ export function SearchTerm({ onSearch }) {
     const handleSearchTerm = (e) => {
         setSearchTerm(e.target.value)
     }
-    const handleSearch = async (e) => {
-        e.preventDefault();
-        try {
-            if (searchTerm === "") {
-                onSearch(null);
-                return;
+
+
+    useEffect(() => {
+        const handleSearch = async () => {
+            try {
+                if (searchTerm === "") {
+                    onSearch(null);
+                    return;
+                }
+                const filteredBooks = await bookService.getFilteredBooks(searchTerm);
+                onSearch(filteredBooks);
+            } catch (error) {
+                console.log(error);
             }
-            const filteredBooks = await bookService.getFilteredBooks(searchTerm)
-            onSearch(filteredBooks)
-        } catch (error) {
-            console.log(error)
-        }
-    }
+        };
+        handleSearch();
+    }, [searchTerm]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+
     return (
-        <form onSubmit={handleSearch}>
+        <form onSubmit={handleSubmit} >
             <input
                 onChange={handleSearchTerm}
                 id="searchTerm"
                 type="text"
                 placeholder="Search"
                 className="w-full md:w-[80vh] px-3 py-2 border bg-green-50 border-green-300 rounded-md shadow-sm focus:outline-none focus:border-green-500 sm:text-sm" />
-        </form>
+        </form >
     )
 }
