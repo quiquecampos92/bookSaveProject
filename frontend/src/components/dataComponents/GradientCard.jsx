f039dd784dcc2d9fc49de8b41fc4b5644c5e410d
“Quique” <quiquecampos92@outlook.com>
2025-03-27 10:22:03 +0100

    fix responsive style

--- frontend/src/components/dataComponents/GradientCard.jsx
+++ frontend/src/components/dataComponents/GradientCard.jsx
@@ -10,33 +10,36 @@ export function GradientCard({ book, handleButton, error }) {
     }
 
     return (
-        <div className="relative w-full max-w-sm last:mb-10 rounded-lg border-2 border-green-500 bg-gradient-to-b hover:bg-gradient-to-t from-green-100 hover:scale-105 shadow-lg hover:shadow-xl transition-all duration-200 p-6">
-            <div className="text-center">
-                <h5 className="text-xl font-bold text-gray-700">
+        <div className="relative w-full max-w-sm rounded-lg border-2 border-green-500 bg-gradient-to-b hover:bg-gradient-to-t from-green-100 hover:scale-105 shadow-lg hover:shadow-xl transition-all duration-200 p-6 flex flex-col justify-start">
+
+            {/* Título */}
+            <div className="text-center mb-2">
+                <h5 className="text-xl font-bold text-gray-700 truncate">
                     {capitalize(book.title)}
                 </h5>
             </div>
 
-            <div className="mt-3 space-y-2">
-                <p className="text-md 2xl:text-xl font-medium text-orange-500">
-                    <span className="text-gray-500 font-semibold">Autor:</span> {book.author}
+            {/* Contenedor con scroll pegado al título */}
+            <div className="h-[100px] overflow-y-auto">
+                <p className="text-md 2xl:text-xl font-medium text-orange-500 break-words">
+                    <span className="text-gray-500 font-semibold">Author:</span> {book.author}
                 </p>
 
-                <p className="text-md 2xl:text-xl font-medium text-orange-500">
-                    <span className="text-gray-500 font-semibold">Reseña:</span> {book.review || "No review available"}
+                <p className="text-md 2xl:text-xl font-medium text-orange-500 break-words">
+                    <span className="text-gray-500 font-semibold">Review:</span> {book.review || "No review available"}
                 </p>
             </div>
 
-            <div className="flex justify-center mt-5">
+            {/* Botón */}
+            <div className="flex justify-center  mt-5">
                 <AddButton
                     handleAddButton={handleButton}
-                    text="Marcar como leído"
+                    text="Mark as read"
                     style="bg-orange-500 text-white px-4 py-2 rounded-lg hover:bg-orange-600 transition"
                     icon={<HiBookmark size={20} />}
                     type="text"
                 />
             </div>
         </div>
-
     );
-};
+}
--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export function Footer() {
     return (
-        <div className='flex items-center justify-center p-8 sm:p-2 text-xs sm:text-sm 2xl:text-lg bg-green-200 h-10'>
+        <div className='flex items-center justify-center p-8 sm:p-2 text-xs sm:text-sm 2xl:text-lg bg-green-200 h-10 mt-3'>
             <h1 className='text-center'>Creado por <strong>Enrique Campos</strong>. Todos los derechos reservados <strong>©</strong> 2025.</h1>
         </div>
     )
