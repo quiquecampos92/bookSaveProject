daac93ce479856a2b72dcea2900b00794ee7e811
“Quique” <quiquecampos92@outlook.com>
2025-03-27 09:34:29 +0100

    fix kep prop

--- frontend/src/components/pages/BooksToRead.jsx
+++ frontend/src/components/pages/BooksToRead.jsx
@@ -46,15 +46,13 @@ export function BooksToRead() {
             {booksToRead.length === 0 ? (
                 <p>No hay libros que quieras leer</p>
             ) : (
-                booksToRead.map((book) => (
-                    <>
-                        <GradientCard
-                            key={book._id}
-                            book={book}
-                            handleButton={() => handleButton(book)}
-                            error={error}
-                        />
-                    </>
+                booksToRead.map((book, index) => (
+                    <GradientCard
+                        key={index}
+                        book={book}
+                        handleButton={() => handleButton(book)}
+                        error={error}
+                    />
                 ))
             )}
         </div>
