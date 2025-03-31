797d96d18dd163da137c8108d7068bc6ca011f04
“Quique” <quiquecampos92@outlook.com>
2025-03-31 21:10:46 +0200

    reset baseurl

--- frontend/src/services/apiClient.js
+++ frontend/src/services/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: "https://booksaveproject.onrender.com/api",
+    baseURL: "/api",
 });
 
 // Interceptor de solicitud: Agrega el token al encabezado
