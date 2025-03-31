0151100f317b6653f73829c3b0dc939301a7c5c8
“Quique” <quiquecampos92@outlook.com>
2025-03-31 20:58:41 +0200

    change baseUrl

--- frontend/src/services/apiClient.js
+++ frontend/src/services/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: "/api",
+    baseURL: "https://booksaveproject.onrender.com/api",
 });
 
 // Interceptor de solicitud: Agrega el token al encabezado
