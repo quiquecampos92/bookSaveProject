d97de016e32b193a4ff7401795bdcebd044a26ac
“Quique” <quiquecampos92@outlook.com>
2025-04-08 17:41:39 +0200

    add validation fields in login and register

--- frontend/src/components/forms/LoginForm.jsx
+++ frontend/src/components/forms/LoginForm.jsx
@@ -7,6 +7,7 @@ import fullLogo from '../../assets/booksavewhiterow.png';
 export function LoginForm() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [checkCredentials, setCheckCredentials] = useState(false);
     const [errorMessage, setErrorMessage] = useState(null);
     const { login } = useContext(AuthContext);
     const navigate = useNavigate();
@@ -15,44 +16,39 @@ export function LoginForm() {
         event.preventDefault();
 
         try {
-            // Llama al servicio de inicio de sesión
-            const userData = await loginService.login({ username, password });
+            const userData = await loginService.login({ username: username.toLowerCase(), password });
 
             // Guarda el usuario y el token en localStorage
             window.localStorage.setItem('loggedUser', JSON.stringify(userData));
             window.localStorage.setItem('loggedUserToken', userData.token);
 
-            // Actualiza el estado de autenticación
             login(userData);
 
-            // Limpia los campos del formulario
             setUsername('');
             setPassword('');
 
-            // Redirige al usuario a la página principal
             navigate('/books');
         } catch (error) {
-            // Maneja errores de autenticación
-            setErrorMessage('Credenciales incorrectas');
+            setCheckCredentials(true);
+            setErrorMessage('Wrong username or password');
             setTimeout(() => {
                 setErrorMessage(null);
+                setCheckCredentials(false);
             }, 5000);
         }
     };
 
     return (
         <>
-            {errorMessage && <p className="text-red-500">{errorMessage}</p>}
-
             <div className="flex justify-center items-center h-screen">
                 <form onSubmit={handleLogin} className="flex flex-col justify-center items-center gap-4 bg-green-200 rounded-xl p-10 bg-opacity-40">
-                    <img src={fullLogo} alt="logo booksave" className="w-64 mb-16 mt-10" />
+                    <img src={fullLogo} alt="logo booksave" className="w-64 mb-10 mt-10" />
 
                     <div>
                         <input
                             type="text"
                             id="username"
-                            className="bg-green-100 bg-opacity-0 text-black border-b-2 border-b-green-500 placeholder-slate-500 focus:border-zinc-50 focus:outline-none p-1"
+                            className={`bg-green-100 bg-opacity-0 text-black border-b-2 ${checkCredentials ? "border-red-500" : "border-b-green-500"} placeholder-slate-500 focus:border-zinc-50 focus:outline-none p-1`}
                             value={username}
                             placeholder="Username"
                             onChange={({ target }) => setUsername(target.value)}
@@ -62,12 +58,13 @@ export function LoginForm() {
                         <input
                             type="password"
                             id="password"
-                            className="bg-green-100 bg-opacity-0 text-black border-b-2 border-b-green-500 placeholder-slate-500 focus:border-zinc-50 focus:outline-none p-1"
+                            className={`bg-green-100 bg-opacity-0 text-black border-b-2 ${checkCredentials ? "border-red-500" : "border-b-green-500"} placeholder-slate-500 focus:border-zinc-50 focus:outline-none p-1`}
                             value={password}
                             placeholder="Password"
                             onChange={({ target }) => setPassword(target.value)}
                         />
                     </div>
+                    {errorMessage && <p className="text-red-500">{errorMessage}</p>}
                     <button type="submit" className="bg-green-600 border-2 border-green-600 rounded-xl text-white py-2 px-7 mt-3 hover:bg-green-500">Login</button>
                     <div className="flex flex-col items-center md:flex-row md:justify-center">
                         <h3>New in Booksave?</h3>
--- frontend/src/components/forms/UserRegisterForm.jsx
+++ frontend/src/components/forms/UserRegisterForm.jsx
@@ -5,24 +5,53 @@ import usersService from "../../services/users";
 
 export function UserRegisterForm() {
     const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
     const [name, setName] = useState('')
     const [lastName, setLastName] = useState('')
     const [password, setPassword] = useState('');
     const [repeatPassword, setRepeatPassword] = useState('')
+    const [checkPass, setcheckPass] = useState(false);
     const [errorMessage, setErrorMessage] = useState(null);
     const navigate = useNavigate();
+
+    const checkPasswords = () => {
+        if (password !== repeatPassword) {
+            setcheckPass(true);
+            setErrorMessage('Las contraseñas no coinciden');
+            setTimeout(() => {
+                setErrorMessage(null);
+                setcheckPass(false);
+            }, 5000);
+            return false;
+        }
+
+        setcheckPass(false);
+        return true;
+    };
 
     const handleSignup = async (event) => {
         event.preventDefault();
 
+        const isPasswordValid = checkPasswords();
+        if (!isPasswordValid) return;
+
+        const userToCreate = {
+            username: username.trim().toLowerCase(),
+            email: email.trim().toLowerCase(),
+            name: name.trim(),
+            lastName: lastName.trim(),
+            password
+        };
+
         try {
-            await usersService.createUser({ username, name, lastName, password });
+            await usersService.createUser(userToCreate);
 
             setUsername('');
-            setName('')
-            setLastName('')
+            setEmail('');
+            setName('');
+            setLastName('');
             setPassword('');
-            setRepeatPassword('')
+            setRepeatPassword('');
 
             navigate('/login');
         } catch (error) {
@@ -33,10 +62,9 @@ export function UserRegisterForm() {
         }
     };
 
+
     return (
         <>
-            {errorMessage && <p className="text-red-500">{errorMessage}</p>}
-
             <div className="flex justify-center items-center h-screen">
                 <form onSubmit={handleSignup} className="flex flex-col justify-center items-center gap-4 bg-green-400 rounded-xl p-10 bg-opacity-50">
                     <img src={fullLogo} alt="logo booksave" className="w-64 mb-16 mt-10" />
@@ -49,6 +77,18 @@ export function UserRegisterForm() {
                             value={username}
                             placeholder="Username"
                             onChange={({ target }) => setUsername(target.value)}
+                            required
+                        />
+                    </div>
+                    <div>
+                        <input
+                            type="email"
+                            id="email"
+                            className="bg-green-100 bg-opacity-0 text-black border-b-2 border-b-green-500 placeholder-slate-500 focus:border-zinc-50 focus:outline-none p-1"
+                            value={email}
+                            placeholder="Email"
+                            onChange={({ target }) => setEmail(target.value)}
+                            required
                         />
                     </div>
                     <div>
@@ -75,7 +115,7 @@ export function UserRegisterForm() {
                         <input
                             type="password"
                             id="password"
-                            className="bg-green-100 bg-opacity-0 text-black border-b-2 border-b-green-500 placeholder-slate-500 focus:border-zinc-50 focus:outline-none p-1"
+                            className={`bg-green-100 bg-opacity-0 text-black border-b-2 ${checkPass ? "border-red-500" : "border-b-green-500"} placeholder-slate-500 focus:border-zinc-50 focus:outline-none p-1`}
                             value={password}
                             placeholder="Password"
                             onChange={({ target }) => setPassword(target.value)}
@@ -85,12 +125,13 @@ export function UserRegisterForm() {
                         <input
                             type="password"
                             id="repeatPassword"
-                            className="bg-green-100 bg-opacity-0 text-black border-b-2 border-b-green-500 placeholder-slate-500 focus:border-zinc-50 focus:outline-none p-1"
+                            className={`bg-green-100 bg-opacity-0 text-black border-b-2 ${checkPass ? "border-red-500" : "border-b-green-500"} placeholder-slate-500 focus:border-zinc-50 focus:outline-none p-1`}
                             value={repeatPassword}
                             placeholder="Repeat Password"
                             onChange={({ target }) => setRepeatPassword(target.value)}
                         />
                     </div>
+                    {errorMessage && <p className="text-red-500">{errorMessage}</p>}
                     <button type="submit" className="bg-green-600 border-2 border-green-600 rounded-xl text-white py-2 px-7 mt-5 hover:bg-green-500">Sign up</button>
                 </form>
             </div>
